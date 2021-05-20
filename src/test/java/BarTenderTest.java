575bcb86610212d236ba167856b93bd0fb80e9eb
ThisHub <jenspieter.mievis@student.intecbrussel.be>
2021-05-20 11:27:27 +0200

    Push Jens

---
+++ src/test/java/BarTenderTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BarTenderTest {
+
+    @Test
+    public void buyBeer(){
+        Assertions.
+    }
+
+
+}
