16aa19aafded37bb68fbd4acf609c8f024233e1b
Guts <bilal.arragragui@student.intecbrussel.be>
2021-05-20 11:14:53 +0200

    Toiletz4Life

--- src/main/java/ToiletMan.java
+++ src/main/java/ToiletMan.java
@@ -18,15 +18,33 @@ public class ToiletMan extends Staff {
 
 
 
-    private void fillToiletStallsWithToiletPaper(int amountOfStalls){
+    private void fillToiletStallsWithToiletPaper(int amountOfStalls) throws Exception {
         //remove the amount of your stock
         //this method will throw a selfmade Exception if you're
         //out of toilet paper
+        if (toiletPaperStock < amountOfStalls) {
+            throw new Exception("No luck use your hands sir");
+
+
+        }
+        this.toiletPaperStock -= amountOfStalls;
+
+
     }
+
+
+
+
 
     private void promoteSafeFun(Customer customer){
         //check if Customer is DTF and if he has no condom
         //AND if he had less than 3 drinks
         //give them a condom
+        if (customer.isDTF() && !customer.isHasCondom() && customer.getDrinksThatTheyDrunk()<3){
+            customer.setHasCondom(true);
+
+
+
+        }
     }
 }
