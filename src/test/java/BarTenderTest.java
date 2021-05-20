f310f58f7718dce5071401b7e17b5e9aa69522cc
ThisHub <jenspieter.mievis@student.intecbrussel.be>
2021-05-20 11:46:51 +0200

    Push Jens

--- src/test/java/BarTenderTest.java
+++ src/test/java/BarTenderTest.java
@@ -4,8 +4,17 @@ import org.junit.jupiter.api.Test;
 public class BarTenderTest {
 
     @Test
-    public void buyBeer(){
-        Assertions.
+    public void buyBeertest(){
+        Customer customer = new Customer("Pearl",14,1.00,true,false);
+        BarTender barTender = new BarTender("Jens", 1.5, 3.5);
+        barTender.buyBeer(customer,2);
+        Assertions.assertEquals(1,customer.getEuros());
+        Assertions.assertEquals(0,customer.getDrinksThatTheyDrunk());
+    }
+
+    @Test
+    public void buyCoctailTest(){
+
     }
 
 
